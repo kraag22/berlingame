2ff8fdf7f68619a040bad0791ba071de4bebe377
Martin Bartusek <mbartusek@section.cz>
2014-03-10 13:51:17 +0100

    change analytics code

--- core/main.php
+++ core/main.php
@@ -253,14 +253,15 @@ if($prave_prihlasen){
 	$page->redir('./intro.php');
 }
 
-$GOOGLE_ANALYTICS = '<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-3082527-2");
-pageTracker._initData();
-pageTracker._trackPageview();
+$GOOGLE_ANALYTICS = '<script>
+  (function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');
+
+  ga(\'create\', \'UA-48831479-1\', \'berlingame.cz\');
+  ga(\'send\', \'pageview\');
+
 </script>';
 
 $GOOGLE_ADWORDS = '<!-- Google Code for registrace Conversion Page -->
