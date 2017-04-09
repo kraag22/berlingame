b7a62cbf5975191dfaf54dc941a69112375ddbd9
Martin Bartusek <bartusek@cryptelo.com>
2017-04-09 15:29:35 +0200

    remove partners from partners page

--- applib/index/partneri.php
+++ applib/index/partneri.php
@@ -1,48 +1,5 @@
 <?php
-$form = new textElement('
-	<h2> Partneři </h2>
-	<div class="partner_ad" style="width:400px;">
-	<a href="http://www.knihyovojne.sk" target="_blank">
-	<img src="http://www.knihyovojne.sk/images/banners/banner400x80.jpg" border="0">
-	</a>
-	</div>
-	
-	<div class="partner_ad" style="width:468px;">
-	<a href="http://www.primat.cz" title="Primát - studijní materiály" target="_blank" >
-	<img border="0" src="http://www.primat.cz/soubory/468_60.png" style="width: 468px; height: 60px; border: 0">
-	</a>
-    </div>
-    
-    <div class="partner_ad" style="width:400px;">
-	<a href="http://www.letadla.tk" title="letadla" target="_blank" >
-	<img border="0" src="images/letadla.jpg" style="width: 400px; height: 84px; border: 0">
-	</a>
-    </div>
-    
-    <div class="partner_ad" style="width:468px;">
-	<a href="http://genesis.ruskur.sk/" title="Genesis - Battle" target="_blank" >
-	<img border="0" src="http://genesis.ruskur.sk/templates/images/bigbanner.gif" style=" border: 0" alt="Genesis - Battle">
-	</a>
-    </div>
-    
-    <div class="partner_ad" style="width:468px;">
-	<a href="http://berlingame.net" target="_blank">
-	<img style="border: 0px; width: 468px; height: 60px;" alt=""
- src="http://berlingame.net/images/bannery/berlingamenet1_468x60.gif"></a>
-    
-    </div>
-    
-    <div class="partner_ad" style="width:468px;">
-    <a href="http://www.boj-o-berlin.cz/"><img style="border: 0px solid ; width: 468px; height: 60px;" 
-    alt="Návody a tipy k online hře berlingame.cz"
-     src="http://www.boj-o-berlin.cz/_media/468_60_boj_o_berlin3.png"></a>
-    </div>
-    
-    
-    <div class="partner_ad" style="width:88px;">
-    <a href="http://www.rimske-imperium.sk" title="Online hra - Rimske Imperium"><img src="http://www.rimske-imperium.sk/grafika/obrazke/ikona.gif" border=0 alt="Online hra - Rimske Imperium" width="88" height="31" ></a>
-    </div>
-    ');
-                        
+$form = new textElement('<h2> Partneři </h2>');
+
 $page->add_element($form, 'obsah');
 ?>
