70a0df8e415af57f8d97777914452b702332b7ac
Martin Bartusek <bartusek@cryptelo.com>
2017-08-15 12:19:18 +0200

    add captcha fix broken registration, fix whitespaces

--- applib/fm_fce/registrace_novacek.php
+++ applib/fm_fce/registrace_novacek.php
@@ -1,74 +1,120 @@
 <?php
 function novy_hrac($myPost){
   global $db,$users_class, $DIR_CONFIG;
-	$nick = sqlsafe($myPost['rn_nick']);
-	$heslo = sqlsafe($myPost['rn_pswd']);
+  $nick = sqlsafe($myPost['rn_nick']);
+  $heslo = sqlsafe($myPost['rn_pswd']);
 
-	$group_id = "hrac";
+  $group_id = "hrac";
 
-	$attribs = array (
-	   		"account_created" => date("Y-m-d H:i:s"), 
-	   		"group_id" => $group_id,
-	  		"account_active" => 1,
-			"ip_adresa_registrace" => sqlsafe($_SERVER['REMOTE_ADDR']),
-	);
+  $attribs = array (
+    "account_created" => date("Y-m-d H:i:s"),
+    "last_activity" => date("Y-m-d H:i:s", 0),
+    "group_id" => $group_id,
+    "account_active" => 1,
+    "ip_adresa_registrace" => sqlsafe($_SERVER['REMOTE_ADDR']),
+  );
 
-	$id = $users_class->new_user($nick,$heslo,$attribs);
-	
-	if (isset($id)){
-		$_SESSION['rn_nick'] = $nick;
-		$_SESSION['rn_heslo'] = $heslo;
-	}
-	//vytvoreni zaznamu v users_hrac
-	$query = "INSERT INTO `users_hrac` (id_user) VALUES ('$id');";
-	$db->DbQuery( $query );
-	
-	//vytvoreni zaznamu v users_stat
-	$query = "INSERT INTO `users_stat` (id_user) VALUES ('$id');";
-	$db->DbQuery( $query );
-	
-	//test+pripsani bonusu za privedeni hrace pres referenci
-	$users_class->reference_test( $id );
+  $id = $users_class->new_user($nick,$heslo,$attribs);
+
+  if (isset($id)){
+    $_SESSION['rn_nick'] = $nick;
+    $_SESSION['rn_heslo'] = $heslo;
+  }
+  //vytvoreni zaznamu v users_hrac
+  $query = "INSERT INTO `users_hrac` (id_user) VALUES ('$id');";
+  $db->DbQuery( $query );
+
+  //vytvoreni zaznamu v users_stat
+  $query = "INSERT INTO `users_stat` (id_user) VALUES ('$id');";
+  $db->DbQuery( $query );
+
+  //test+pripsani bonusu za privedeni hrace pres referenci
+  $users_class->reference_test( $id );
 }
 
 
 function heslo(){
-	if ($_POST['rn_pswd'] != $_POST['rn_pswd_again'])
-	{
-		return "Zadaná hesla jsou různá. Zadejte je prosím znovu"; 
-	}
-	
-	if(strlen($_POST['rn_pswd'])==0){
-		return "Zadejte heslo"; 
-	}
+  if ($_POST['rn_pswd'] != $_POST['rn_pswd_again'])
+  {
+    return "Zadaná hesla jsou různá. Zadejte je prosím znovu";
+  }
 
-	return false; // uspech
+  if(strlen($_POST['rn_pswd'])==0){
+    return "Zadejte heslo";
+  }
+
+  return false; // uspech
 }
 
 function unikatni_login(){
-	global $db;
-	
-	//zabraneni vstupu html kodu
-	if((strpos($_POST['rn_nick'],'<')!= 0)||
-		(strpos($_POST['rn_nick'],'>')!= 0)||
-		(strpos($_POST['rn_nick'],'\'')!= 0)||
-		(strpos($_POST['rn_nick'],'\"')!= 0)){
-			return "Login nesmí obsahovat znaky <,>,',\"";
-		}
-		
-	//omezeni delky
-	if(strlen($_POST['rn_nick'])>15){
-		return "Zadejte heslo"; 
-	}	
-	
-	//kontrola unikatnosti
-	$db->Query("SELECT `id` FROM users_sys 
-						  WHERE `login`='".sqlsafe($_POST['rn_nick'])."'");
-	if ($db->GetResult()) {
+  global $db;
 
-			return "Uživatel s tímto loginem už existuje. Zvolte si jiný.";
-		}
-	return false; // uspech
+  $result = checkRecaptcha();
+
+  if ($result) {
+    return $result;
+  }
+
+  //zabraneni vstupu html kodu
+  if((strpos($_POST['rn_nick'],'<')!= 0)||
+    (strpos($_POST['rn_nick'],'>')!= 0)||
+    (strpos($_POST['rn_nick'],'\'')!= 0)||
+    (strpos($_POST['rn_nick'],'\"')!= 0)){
+      return "Login nesmí obsahovat znaky <,>,',\"";
+    }
+
+  //omezeni delky
+  if(strlen($_POST['rn_nick'])>15){
+    return "Zadejte heslo";
+  }
+
+  //kontrola unikatnosti
+  $db->Query("SELECT `id` FROM users_sys
+              WHERE `login`='".sqlsafe($_POST['rn_nick'])."'");
+  if ($db->GetResult()) {
+    return "Uživatel s tímto loginem už existuje. Zvolte si jiný.";
+  }
+  return false; // uspech
+}
+
+function checkRecaptcha() {
+  global $RECAPTCHA_SECRET;
+
+  $url = 'https://www.google.com/recaptcha/api/siteverify';
+  $data = array(
+    'secret' => $RECAPTCHA_SECRET,
+    'response' => $_REQUEST['g-recaptcha-response'],
+    'remoteip' => $_SERVER['REMOTE_ADDR']
+    );
+
+  $options = array(
+      'http' => array(
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query($data)
+      )
+  );
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+
+  if ($result === false) {
+    return 'Chyba pripojeni. Zkuste se registrovat pozdeji.';
+  }
+  else {
+    $result = json_decode($result);
+    if (!$result->success) {
+      return 'Musíte kliknout na reCaptcha pod tlačítkem "registrovat".';
+    }
+    else if ($result->success === false) {
+      return $result->error-codes;
+    }
+    else if ($result->success === true) {
+      return false;
+    }
+    else {
+      return 'unknown error';
+    }
+  }
 }
 
 ?>
--- applib/registrace/reg_novacek.php
+++ applib/registrace/reg_novacek.php
@@ -1,22 +1,26 @@
 <?php
-
+  global $RECAPTCHA_SITEKEY;
 	if ($fm_class->catched('registrace_novacek'))
   		{
 			if (isset($_SESSION['rn_nick']) && isset($_SESSION['rn_heslo'])){
 				$users_class->log_user($_SESSION['rn_nick'], $_SESSION['rn_heslo']);
 			}
-  			
+
 			$page->redir($DIRECTORY . 'intro.php?registrace=ano');
   		}
-  		
+
 
 	$text = '<div class="reg_prvni">';
-	$text .= '<strong>Předím než začnete hrát, bychom vám rádi doporučili 
-nahlédnutí do <a href="index.php?section=napoveda">nápovědy</a>. Jde o obrázkové tutoriály, které 
+  $text .= "<script src='https://www.google.com/recaptcha/api.js'></script>";
+	$text .= '<strong>Předím než začnete hrát, bychom vám rádi doporučili
+nahlédnutí do <a href="index.php?section=napoveda">nápovědy</a>. Jde o obrázkové tutoriály, které
 prolétnete za pár minut. </strong>';
-	$text .= $fm_class->create_form('registrace_novacek', null, null, $page);
+  $form = $fm_class->create_form('registrace_novacek', null, null, $page);
+  $captcha = "<div class='g-recaptcha' data-sitekey='".$RECAPTCHA_SITEKEY."'></div>";
+  $form = str_replace('</form>', $captcha . '</form>', $form);
+	$text .= $form;
 	$text .= '<strong>Svou registrací potvrzujete, že jste si přečetl(a) <a href="index.php?section=pravidla" target="_blank">
-	pravidla chování ve hře</a> a 
+	pravidla chování ve hře</a> a
 	budete se jimi řídit.</strong>';
 	$text .= '</div>';
 
