f56e326f0d69cccbcf1811875ea00e15bc8b81de
Martin Bartusek <bartusek@cryptelo.com>
2021-02-07 15:58:13 +0100

    show

--- skins/default/index/printer.php
+++ skins/default/index/printer.php
@@ -148,7 +148,7 @@ echo "</div>\n" .
 	}
 
 if (isset($page->private_texts['prepocet_info'])){
-  echo "<p hidden>\n";
+  echo "<p>\n";
   echo "PREPOCET_STATUS:" . $page->private_texts['prepocet_info'];
   echo "</p>\n";
 }
